dddf90cb572261826f28a814f174ed2d5106c0f1
Keshuai Xu <i@xks.im>
2015-04-22 17:41:31 -0400

    tfufyyy'

---
+++ Pins.h
@@ -0,0 +1,13 @@
+#ifndef Pins_h
+#define Pins_h
+#define PIN_ENCODER_LEFT_A	2
+#define	PIN_ENCODER_LEFT_B	22
+#define PIN_ENCODER_RIGHT_A	3
+#define PIN_ENCODER_RIGHT_B	23
+#define PIN_MOTOR_LEFT_FWD	24
+#define PIN_MOTOR_LEFT_REV	25
+#define PIN_MOTOR_LEFT_PWM	4
+#define PIN_MOTOR_RIGHT_FWD	26
+#define PIN_MOTOR_RIGHT_REV	27
+#define PIN_MOTOR_RIGHT_PWM	5
+#endif
